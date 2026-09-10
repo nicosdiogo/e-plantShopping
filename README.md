288c42ea4a6510a6887128c6482a664bde86ca26
Nicola João <nicosdiogo@gmail.com>
2026-09-10 15:27:10 +0100

    done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # coding-project-template
+
+Projecto final do curso de React da IMB
--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import ProductList from './ProductList';
 import './App.css';
 import AboutUs from './AboutUs';
+import addItem from './CartItem.jsx'; // Import the addItem action from CartItem.jsx
 
 function App() {
   
@@ -15,6 +16,18 @@ function App() {
   const handleHomeClick = () => {
     setShowProductList(false);
   };
+
+  const [addedToCart, setAddedToCart] = useState({});
+
+
+    const handleAddToCart = (product) => {
+  dispatch(addItem(product)); // Dispatch the action to add the product to the cart (Redux action)
+
+  setAddedToCart((prevState) => ({ // Update the local state to reflect that the product has been added
+    ...prevState, // Spread the previous state to retain existing entries
+    [product.name]: true, // Set the current product's name as a key with value 'true' to mark it as added
+  }));
+};
 
   return (
     <div className="app-container">
